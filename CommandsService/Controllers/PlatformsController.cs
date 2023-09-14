b6644ac0801fb4d1775af0982428fd817dab85f0
davidgavrilut <davidgavrilut1@gmail.com>
2023-09-14 18:02:49 +0300

    commands service controller and platform service sync data

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        { }
+
+        [HttpPost]
+        public ActionResult TestInbound()
+        {
+            Console.WriteLine("--> Inbound Post # Command Service");
+            return Ok("Inbound test ok from Platforms Controller"); 
+        }
+
+    }
+}
---
+++ PlatformService/SyncDataServices/Http/HttpCommandDataClient.cs
@@ -0,0 +1,21 @@
+﻿using PlatformService.Dtos;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PlatformService.SyncDataServices.Http
+{
+    public class HttpCommandDataClient : ICommandDataClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public HttpCommandDataClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Task SendPlatformToCommand(PlatformReadDto plat)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
